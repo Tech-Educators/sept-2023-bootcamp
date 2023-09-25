753b283851d9c195c47e1f7a8ab52fc29dc71c14
Tim Smith <tim.smith2814@gmail.com>
2023-09-25 15:42:37 +0100

    Add lab 6 demo

---
+++ 201/class-06/lab/demo/app.js
@@ -0,0 +1,43 @@
+const hours = [
+  "6am",
+  "7am",
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+  "6pm",
+  "7pm",
+];
+
+// give a random number between two numbers
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// create our first shop
+const seattle = {
+  location: "seattle",
+  minCust: 23,
+  maxCust: 65,
+  avgCookiesPerCust: 6.3,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalCookieSold: 0,
+  calculateSales: function () {
+    // add a random number to the customersPerHour array
+    for (let i = 0; i < hours.length; i++) {
+      const randNum = randomNumber(this.minCust, this.maxCust);
+      this.customersPerHour.push(randNum);
+      this.cookiesPerHour.push(randNum * this.avgCookiesPerCust);
+    }
+  },
+};
+
+seattle.calculateSales();
+console.log(seattle);
