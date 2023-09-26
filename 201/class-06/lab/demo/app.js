96e4f59e0549dda240ae09579c40a7bc2adae49e
Wollivan <tim.smith2814@gmail.com>
2023-09-26 09:53:28 +0100

    Add class 6 demo part 2

--- 201/class-06/lab/demo/app.js
+++ 201/class-06/lab/demo/app.js
@@ -22,7 +22,7 @@ function randomNumber(min, max) {
 
 // create our first shop
 const seattle = {
-  location: "seattle",
+  location: "Seattle",
   minCust: 23,
   maxCust: 65,
   avgCookiesPerCust: 6.3,
@@ -30,14 +30,66 @@ const seattle = {
   cookiesPerHour: [],
   totalCookieSold: 0,
   calculateSales: function () {
-    // add a random number to the customersPerHour array
     for (let i = 0; i < hours.length; i++) {
       const randNum = randomNumber(this.minCust, this.maxCust);
       this.customersPerHour.push(randNum);
-      this.cookiesPerHour.push(randNum * this.avgCookiesPerCust);
+      this.cookiesPerHour.push(Math.floor(randNum * this.avgCookiesPerCust));
+    }
+  },
+};
+
+const tokyo = {
+  location: "Tokyo",
+  minCust: 3,
+  maxCust: 24,
+  avgCookiesPerCust: 1.2,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalCookieSold: 0,
+  calculateSales: function () {
+    for (let i = 0; i < hours.length; i++) {
+      const randNum = randomNumber(this.minCust, this.maxCust);
+      this.customersPerHour.push(randNum);
+      this.cookiesPerHour.push(Math.floor(randNum * this.avgCookiesPerCust));
     }
   },
 };
 
 seattle.calculateSales();
-console.log(seattle);
+tokyo.calculateSales();
+
+// get the element on the page with the id salesData
+const salesData = document.getElementById("salesData");
+
+// add the title for the location
+const seattleH2 = document.createElement("h2")
+seattleH2.textContent = seattle.location;
+salesData.appendChild(seattleH2)
+
+// create a list to show the cookies sold at each hour
+const seattleUl = document.createElement("ul");
+// loop through out data and for each item create an <li>
+for (let i = 0; i < hours.length; i++) {
+  const li = document.createElement("li");
+  li.textContent = `${hours[i]}: ${seattle.cookiesPerHour[i]} cookies`
+  seattleUl.appendChild(li)
+}
+
+salesData.appendChild(seattleUl)
+
+
+// add the title for the location
+const tokyoH2 = document.createElement("h2")
+tokyoH2.textContent = tokyo.location;
+salesData.appendChild(tokyoH2)
+
+// create a list to show the cookies sold at each hour
+const tokyoUl = document.createElement("ul");
+// loop through out data and for each item create an <li>
+for (let i = 0; i < hours.length; i++) {
+  const li = document.createElement("li");
+  li.textContent = `${hours[i]}: ${tokyo.cookiesPerHour[i]} cookies`
+  tokyoUl.appendChild(li)
+}
+
+salesData.appendChild(tokyoUl)
