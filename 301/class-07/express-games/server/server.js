a38a5e34d1f368b16812d35ccd1b5e99ce9bb8b5
Tim Smith <tim.smith2814@gmail.com>
2023-10-31 11:47:37 +0000

    Add more express

--- 301/class-07/express-games/server/server.js
+++ 301/class-07/express-games/server/server.js
@@ -16,7 +16,34 @@ const app = express();
 // ACTIVATE CORS - don't forget to put the () after cors
 app.use(cors());
 
-// here is where our custom code will live
+// import our json data
+const data = require("./games.json");
+
+// this function returns an array with all the games that match the year parameter
+function findGameByYear(year) {
+  return data.filter((game) => game.year == year || !year);
+}
+
+// function findGameByYear(year) {
+//   return data.filter(function (game) {
+//     return game.year == year || !year;
+//   });
+// }
+
+// Endpoint for /
+// .get() takes 2 parameters
+// the first is the route
+// the second is the function to run whenever the route is visited
+app.get("/", (request, response) => {
+  response.json("You are looking at my root route... how roude!");
+});
+
+// Endpoint for /games
+// localhost:8080/games?year=1999
+app.get("/games", (request, response) => {
+  const games = findGameByYear(request.query.year);
+  response.json(games);
+});
 
 // start the server on our PORT, and give it a console log so we can see its working
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
